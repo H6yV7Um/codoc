43bc8321f1d3850bedb634c827b2fa9a446b96ce
xiangguang <xiangguang@staff.weibo.com>
2017-08-04 15:47:11 +0800

    modify script

--- docAndTips/wmgh/服务器管理脚本/本地/downloadMysql.sh
+++ docAndTips/wmgh/服务器管理脚本/本地/downloadMysql.sh
@@ -8,17 +8,17 @@ cd /backup/mysql
 if [ "${isMonday}" == 1 ] && ! [ -e "$fileName" ]; then
 
    /bin/sleep 6300
-   /usr/bin/wget --ftp-user=autoget --ftp-password=weareb1Est -o ./words_tool_$logFile  ftp://iwmgh.com/words_tool_$fileName
+   /usr/bin/wget --ftp-user=autoget --ftp-password=mypw -o ./words_tool_$logFile  ftp://host.com/words_tool_$fileName
 
-   /usr/bin/wget --ftp-user=autoget --ftp-password=weareb1Est -o ./auth_$logFile  ftp://iwmgh.com/auth_$fileName
+   /usr/bin/wget --ftp-user=autoget --ftp-password=mypw -o ./auth_$logFile  ftp://host.com/auth_$fileName
 
-   /usr/bin/wget --ftp-user=autoget --ftp-password=weareb1Est -o ./gtd_$logFile  ftp://iwmgh.com/gtd_$fileName
+   /usr/bin/wget --ftp-user=autoget --ftp-password=mypw -o ./gtd_$logFile  ftp://host.com/gtd_$fileName
 
-   /usr/bin/wget --ftp-user=autoget --ftp-password=weareb1Est -o ./studymate_$logFile  ftp://iwmgh.com/studymate_$fileName
+   /usr/bin/wget --ftp-user=autoget --ftp-password=mypw -o ./studymate_$logFile  ftp://host.com/studymate_$fileName
 
-   /usr/bin/wget --ftp-user=autoget --ftp-password=weareb1Est -o ./super_memory_$logFile  ftp://iwmgh.com/super_memory_$fileName
+   /usr/bin/wget --ftp-user=autoget --ftp-password=mypw -o ./super_memory_$logFile  ftp://host.com/super_memory_$fileName
 
-   /usr/bin/wget --ftp-user=autoget --ftp-password=weareb1Est -o ./inactive_data_$logFile  ftp://iwmgh.com/inactive_data_$fileName
+   /usr/bin/wget --ftp-user=autoget --ftp-password=mypw -o ./inactive_data_$logFile  ftp://host.com/inactive_data_$fileName
 
 fi
 
