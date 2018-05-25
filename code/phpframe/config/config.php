b7fc644c832d2b37cc23160d8cca31dc7e5b729f
xiangguang <xiangguang@staff.weibo.com>
2018-05-25 17:48:18 +0800

    updated

---
+++ code/phpframe/config/config.php
@@ -0,0 +1,18 @@
+<?php
+
+
+return array(
+	// 数据库配置
+	'db' => array(
+		'host'	=> 'localhost',
+		'username'	=> 'root',
+		'password'	=> '123456',
+		'dbname'	=> 'project',
+	),
+
+	// 默认控制器和操作名
+	'defaultController' => 'Item',
+	'defaultAction'		=> 'index',
+);
+
+
